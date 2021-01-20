0848a4aae172a03b5e94feeebddd75555d03d2e1
sgloayza <sgloayza@espol.edu.ec>
2021-01-20 13:06:16 -0500

    Inizializar

---
+++ src/tallerpruebas/Casos.java
@@ -0,0 +1,14 @@
+package tallerpruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Casos {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
