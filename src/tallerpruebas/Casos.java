b3e03c706d3acd6d62270e186de2c4ab316a9959
rpcedill <rpcedill@espol.edu.ec>
2021-01-21 00:56:56 -0500

    Moneda no es USD, es supervisor

--- src/tallerpruebas/Casos.java
+++ src/tallerpruebas/Casos.java
@@ -1,17 +1,11 @@
 package tallerpruebas;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import junit.framework.TestCase;
 
-public class Casos {
+public class Casos extends TestCase{
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
-        @Test
+    @Test
 	public void test(){
 		//Moneda USD, es supervisor, mes par
 		Employee instance=new Employee(400,"USD",10, EmployeeType.Supervisor);
@@ -27,6 +21,20 @@ public class Casos {
 		assertEquals(expected , instance.cs() ,0);
 	}
 	
+	@Test
+	public void testCs_noUsdSupervisor() {
+		//Moneda no es USD, es supervisor- cs
+		Employee em = new Employee(400, "MXN",5, EmployeeType.Supervisor);
+		float resultadoEsperado = 446.0f;
+		assertEquals(resultadoEsperado,em.cs(),0.1);
+	}
+	@Test
+	public void testCalculateYearBonus_noUsdSupervisor() {
+		//Moneda no es USD, es supervisor- CalculateYearBonus
+		Employee em = new Employee(400, "MXN",5, EmployeeType.Supervisor);
+		float resultadoEsperado = 573.0f;
+		assertEquals(resultadoEsperado,em.CalculateYearBonus(),0.1);
+	}
 	
 
 }
