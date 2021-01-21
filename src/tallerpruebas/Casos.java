5d575a6a1d0e7c0ddda701dcacaf5b7b4e138b8e
wajogonz <wajogonz@espol.edu.ec>
2021-01-20 22:56:30 -0500

    Moneda USD, supervisor y mes impar

--- src/tallerpruebas/Casos.java
+++ src/tallerpruebas/Casos.java
@@ -12,11 +12,21 @@ public class Casos {
 	}
 
         @Test
-	public void Test(){
-		//mes par
+	public void test(){
+		//Moneda USD, es supervisor, mes par
 		Employee instance=new Employee(400,"USD",10, EmployeeType.Supervisor);
 		float expected=403.5f;
 		assertEquals(expected , instance.cs() ,0);
 	}
+
+	@Test	
+	public void Test(){
+		//Moneda USD, es supervisor, mes impar
+		Employee instance=new Employee(400,"USD", 10, EmployeeType.Supervisor);
+		float expected=467.83334f;
+		assertEquals(expected , instance.cs() ,0);
+	}
+	
+	
 
 }
